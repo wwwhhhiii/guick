9cc24d805e3b161af32261eda12f94b22e8d8481
Alexander <alexandr.s.trushnikov@gmail.com>
2025-06-16 16:29:42 +0500

    Added connect initialization logic

--- guick/main.go
+++ guick/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"flag"
 	"log"
+	"net"
 	"net/http"
+	"net/url"
 	"os"
 	"os/signal"
 
@@ -52,11 +54,36 @@ func (wsh *wsServeHandler) serveWs(w http.ResponseWriter, r *http.Request) {
 	wsh.hub.register <- &ReqClient{peerId: peerId, conn: conn}
 }
 
+func connect(addr string, ourPeerId uuid.UUID) (*ReqClient, error) {
+	url := url.URL{Scheme: "ws", Host: addr, Path: "/ws"}
+	conn, _, err := websocket.DefaultDialer.Dial(url.String(), nil)
+	if err != nil {
+		log.Println("peer connect err:", err)
+		return nil, err
+	}
+
+	err = conn.WriteMessage(websocket.TextMessage, []byte(ourPeerId.String()))
+	if err != nil {
+		log.Println("peer id write err:", err)
+		return nil, err
+	}
+
+	_, msg, err := conn.ReadMessage()
+	if err != nil {
+		log.Println("read peer id err:", err)
+		return nil, err
+	}
+
+	// might panic here, need validation that msg is uuid
+	peerId := uuid.UUID(msg)
+	return &ReqClient{peerId: peerId, conn: conn}, nil
+}
+
 func main() {
 	flag.Parse()
 
-	clientId := uuid.New()
-	log.Println("client id:", clientId)
+	ourPeerId := uuid.New()
+	log.Println("client id:", ourPeerId)
 	hub := newHub()
 	defer hub.Shutdown()
 	interrupt := make(chan os.Signal, 1)
@@ -64,13 +91,13 @@ func main() {
 
 	go hub.Run(interrupt)
 
-	wsHandler := &wsServeHandler{hub: hub, peerId: clientId}
+	wsHandler := &wsServeHandler{hub: hub, peerId: ourPeerId}
 	http.HandleFunc("/ws", wsHandler.serveWs)
 	log.Println("running server on", *addr)
 	go http.ListenAndServe(*addr, nil)
 
 	app := app.New()
-	window := app.NewWindow("Guick")
+	window := app.NewWindow("Guic")
 	window.Resize(fyne.NewSize(500, 500))
 
 	clientEntry := widget.NewEntry()
@@ -79,11 +106,25 @@ func main() {
 	comWindow := container.New(
 		layout.NewVBoxLayout(),
 		clientEntry,
-		widget.NewButton("Clear", func() {
-			if clientEntry.Text == "" {
+		widget.NewButton("Connect", func() {
+			log.Print(clientEntry.Text)
+			// TODO validate address
+			host, _, err := net.SplitHostPort(clientEntry.Text)
+			if err != nil {
+				log.Println("invalid peer address:", clientEntry.Text)
 				return
 			}
-			log.Print(clientEntry.Text)
+			peerIP := net.ParseIP(host)
+			if peerIP == nil {
+				log.Println("invalid peer address:", clientEntry.Text)
+				return
+			}
+			req, err := connect(clientEntry.Text, ourPeerId)
+			if err != nil {
+				// TODO do some error pop-up
+				return
+			}
+			hub.register <- req
 			clientEntry.SetText("")
 		}),
 	)
@@ -91,11 +132,11 @@ func main() {
 		nil, nil, nil, nil, comWindow,
 	)
 
-	go func() {
-		for msg := range hub.recvMessage {
-			fyne.DoAndWait(func() { clientEntry.SetText(msg.txt) })
-		}
-	}()
+	// go func() {
+	// 	for msg := range hub.recvMessage {
+	// 		fyne.DoAndWait(func() { clientEntry.SetText(msg.txt) })
+	// 	}
+	// }()
 
 	window.SetContent(content)
 	window.Show()
